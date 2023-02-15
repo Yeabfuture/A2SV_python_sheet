f9da560b24cf8cd5f080ca2efa2e2ba8884fcb48
Yeabsira Tesfaye <122813658+Yeabfuture@users.noreply.github.com>
2023-02-15 16:13:21 +0300

    Create python_division.py

---
+++ python_division.py
@@ -0,0 +1,6 @@
+first_number = int(input(''))
+second_number = int(input(''))
+absolute_div = first_number // second_number
+flot_div = first_number / second_number
+print(absolute_div)
+print(flot_div)
