45a549ab0118a684cea535c1af6c34460a7fcac6
Yeabsira Tesfaye <122813658+Yeabfuture@users.noreply.github.com>
2023-02-15 16:07:30 +0300

    arithmetic operators commit

---
+++ arithmetic_operators.py
@@ -0,0 +1,8 @@
+first_number = int(input())
+second_number = int(input())
+sum_of_numbers = first_number + second_number
+difference_of_numbers = first_number - second_number
+product_of_numbers = first_number * second_number
+print(sum_of_numbers)
+print(difference_of_numbers)
+print(product_of_numbers)
